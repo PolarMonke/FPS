a21b7bcb41dca63e53153e3ca9d7d9fe5e62453f
Raman <polarmonkey000@gmail.com>
2024-12-18 18:41:55 +0300

    Fixed chill guy and cottage cheese gun not despawning

--- Assets/Scripts/DummySpawn.cs
+++ Assets/Scripts/DummySpawn.cs
@@ -1,31 +1,56 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
 public class DummySpawn : MonoBehaviour
 {
     public GameObject dummyPrefab;
     public GameObject gunPrefab;
 
+    private List<GameObject> guns = new List<GameObject>();
+    private List<GameObject> dummies = new List<GameObject>();
+
     public Vector3 dummySpawnPosition;
 
     public void SpawnDummy()
     {
         dummySpawnPosition = transform.position;
-        Instantiate(dummyPrefab, dummySpawnPosition, transform.rotation);    
+        GameObject newDummy = Instantiate(dummyPrefab, dummySpawnPosition, transform.rotation);
+        dummies.Add(newDummy);
     }
+
     public void RemoveDummy()
     {
-        GameObject dummy = GameObject.FindGameObjectWithTag("Dummy");
-        Destroy(dummy);
+        if (dummies.Count > 0)
+        {
+            GameObject dummy = dummies[0];
+            if (dummy != null)
+            {
+                Destroy(dummy);
+                dummies.RemoveAt(0);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("No dummies to remove!");
+        }
     }
     public void SpawnGun()
     {
         dummySpawnPosition = transform.position;
-        Instantiate(gunPrefab, dummySpawnPosition, transform.rotation);
+        GameObject newGun = Instantiate(gunPrefab, dummySpawnPosition, transform.rotation);
+        guns.Add(newGun);
     }
     public void DestroyGun()
     {
-        GameObject gun = GameObject.FindGameObjectWithTag("SpecialWeapon");
-        gun.GetComponent<Weapon>().WeaponDespawn();
-        Destroy(gun);
+        if (guns.Count > 0) {
+            GameObject gun = guns[0];
+            if (gun != null && gun.GetComponent<Weapon>() != null) {
+                gun.GetComponent<Weapon>().WeaponDespawn();
+                Destroy(gun);
+                guns.RemoveAt(0);
+            }
+        }
+
     }
 }
